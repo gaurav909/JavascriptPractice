7622c4b960f76b939e5e0db0cdbfd1a6c58454a5
gaurav909 <gaurav150702@gmail.com>
2024-02-07 23:32:44 +0530

    Ternary operator

--- index.js
+++ index.js
@@ -10,8 +10,28 @@
 // console.log(a||getName())
 
 //Template Literals
-let firstName='Gaurav'
-let secondName='Singh'
-let prefix='Mr'
+// let firstName='Gaurav'
+// let secondName='Singh'
+// let prefix='Mr'
 
-console.log(`${prefix} ${firstName} ${secondName}`)
+// console.log(`${prefix} ${firstName} ${secondName}`)
+
+//ternary Operator
+
+let returnage=false;
+
+function getAgeInfo(age){
+    return `This Person is ${age} Year  old`
+}
+
+function getCity(){
+    return 'This person is from USA'
+
+}
+// if(returnage){
+//     console.log(getAgeInfo(30))
+// }else{
+//     console.log(getCity())
+// }
+
+returnage?console.log(getAgeInfo(50)):console.log(getCity())
